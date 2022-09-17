d22060b63ed8c6676c7cb0748f2102ea67d89d50
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-09-17 09:25:05 -0300

    Criando o projeto.

---
+++ bytebank/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Boas Vindas ao seu banco, ByteBank!");
+
+string titular = "Andre Bessa";
+string conta = "10123-x";
+int numero_agencia = 23;
+string nome_agencia = "Agencia Central";
+double saldo = 93.50;
+
+Console.WriteLine("Titular: " + titular);
+Console.WriteLine("Conta: " + conta);
+Console.WriteLine("Número Agencia: " + numero_agencia);
+Console.WriteLine("Número da Agencia: " + nome_agencia);
+Console.WriteLine("Saldo: " + saldo);
+
+Console.ReadKey();
