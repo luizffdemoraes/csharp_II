c5582eb8ff8d94e9aae2b6e1daee0baa1898bc95
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-09-17 10:08:26 -0300

    Criando um objeto

--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -1,15 +1,21 @@
-﻿Console.WriteLine("Boas Vindas ao seu banco, ByteBank!");
+﻿using bytebank;
 
-string titular = "Andre Bessa";
-string conta = "10123-x";
-int numero_agencia = 23;
-string nome_agencia = "Agencia Central";
-double saldo = 93.50;
 
-Console.WriteLine("Titular: " + titular);
-Console.WriteLine("Conta: " + conta);
-Console.WriteLine("Número Agencia: " + numero_agencia);
-Console.WriteLine("Número da Agencia: " + nome_agencia);
-Console.WriteLine("Saldo: " + saldo);
+Console.WriteLine("Boas Vindas ao seu banco, ByteBank!");
+
+ContaCorrente conta1 = new ContaCorrente();
+conta1.titular = "André Silva";
+conta1.conta = "10123-X";
+conta1.numero_agencia = 23;
+conta1.nome_agencia = "Agência Central";
+conta1.saldo = 100;
+
+ContaCorrente conta2 = new ContaCorrente();
+
+Console.WriteLine("Titular: " + conta1.titular);
+Console.WriteLine("Conta: " + conta1.conta);
+Console.WriteLine("Número Agencia: " + conta1.numero_agencia);
+Console.WriteLine("Número da Agencia: " + conta1.nome_agencia);
+Console.WriteLine("Saldo: " + conta1.saldo);
 
 Console.ReadKey();
