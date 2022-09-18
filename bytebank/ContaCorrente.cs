52d0eeb1c4a818d8fd15e745a762c878ba051372
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-09-17 23:17:57 -0300

    Comportamento de uma classe e desafio.

--- bytebank/ContaCorrente.cs
+++ bytebank/ContaCorrente.cs
@@ -7,5 +7,52 @@
         public int numero_agencia;
         public string nome_agencia;
         public double saldo;
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+        }
+        public void Depositar (double valor) 
+        { 
+            saldo = saldo + valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+        }
+        public void ExibirDadosDaConta()
+        {
+            Console.WriteLine("Titular :" + titular);
+            Console.WriteLine("Conta :" + conta);
+            Console.WriteLine("Número Agência :" + numero_agencia);
+            Console.WriteLine("Nome Agência :" + nome_agencia);
+            Console.WriteLine("Saldo: " + saldo);
+        }
     }
 }
--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -13,24 +13,41 @@ conta1.nome_agencia = "Agência Central";
 conta1.saldo = 100;
 
 ContaCorrente conta2 = new ContaCorrente();
-conta1.titular = "André Silva";
-conta1.conta = "10123-X";
-conta1.numero_agencia = 23;
-conta1.nome_agencia = "Agência Central";
-conta1.saldo = 100;
+conta2.titular = "André Silva";
+conta2.conta = "10123-X";
+conta2.numero_agencia = 23;
+conta2.nome_agencia = "Agência Central";
+conta2.saldo = 100;
+
+Console.WriteLine("Saldo da Amanda pré-transferência: " + conta2.saldo);
+Console.WriteLine("Saldo do André pré-transferência: " + conta1.saldo);
+bool transferencia = conta1.Transferir(50, conta2);
+Console.WriteLine("Transferência realizada com sucesso? " + transferencia);
+Console.WriteLine("Saldo da Amanda pós-transferência: " + conta2.saldo);
+Console.WriteLine("Saldo do André pós-transferência: " + conta1.saldo);
+
+conta2.ExibirDadosDaConta();
+
+//Console.WriteLine("Saldo da Amanda pré-saque: " + conta2.saldo);
+//bool saque = conta2.Sacar(50);
+//Console.WriteLine("Saque realizado? "+saque);
+
+//Console.WriteLine("Saldo da Amanda pós-saque: " + conta2.saldo);
+
+////conta2.Depositar(60);
+//Console.WriteLine("Saldo da Amanda pós-depósito: " + conta2.saldo);
 
 // Ao fazer isso ambos ampontaram para o endereço em memoria da conta 2
-conta1 = conta2;
+//conta1 = conta2;
 
 // Verificação de tipo por referencia todo objedo criado e armazenado em um espaço em memoria e ao realizar a verificação são divergentes pois são alocados em endereços diferentes
-Console.WriteLine(conta1 == conta2);
+//Console.WriteLine(conta1 == conta2);
 
 // Verificação de tipo por valor
-double valor = 100;
-Console.WriteLine(valor == conta1.saldo);
+//double valor = 100;
+//Console.WriteLine(valor == conta1.saldo);
 
-
-ContaCorrente conta3 = new ContaCorrente();
+//ContaCorrente conta3 = new ContaCorrente();
 
 //console.writeline("titular: " + conta1.titular);
 //console.writeline("conta: " + conta1.conta);
